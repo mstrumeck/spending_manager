4a28253e4e1417513721ddc4fe26fb2fe2274b0a
Kleju0011 <michal.strumecki@wp.pl>
2016-11-03 20:52:27 +0100

    2016-11-03 20:52

--- core_sm/views.py
+++ core_sm/views.py
@@ -7,9 +7,10 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 from django.utils.safestring import mark_safe
 from bokeh.resources import CDN
+from bokeh.charts import Bar, output_file, show
+from bokeh.sampledata.autompg import autompg as df
 import calendar
-import datetime
-import pprint
+from math import pi
 
 def costs_list(request, datemonthyear_slug = None):
     datemonthyear = None
@@ -25,7 +26,7 @@ def costs_list(request, datemonthyear_slug = None):
 def costs_month_detail(request, id, slug):
     cost = get_object_or_404(Cost, id=id, slug=slug)
     return render(request, 'core_sm/costs/detail.html', {'cost': cost})
-#{% url 'core_sm:stats_detail' %}
+
 def costs_stats(request):
     if request.method == "GET":
         form = data_generate_form()
@@ -40,15 +41,9 @@ def stats_detail(request, year, month):
     mr = calendar.monthrange(int(year), int(month))
     x = [x for x in range(mr[1]+1)][1:]
     y = []
-    for item in Cost.objects.values('publish'):
-        for day in x:
-            if item['publish'].date() == datetime.date(int(year), int(month), day):
-                y.append(Cost.objects.filter(publish__year=year, publish__month=month, publish__day=day).aggregate(Sum('value'))['value__sum'])
-                
-            else:
-                y.append(0)
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-    p.line(x, y, legend="Temp.", line_width=2)
+    for day in x:
+        y.append(Cost.objects.filter(publish__year=year, publish__month=month, publish__day=day).aggregate(Sum('value'))['value__sum'])
+    p = Bar(x, y, values='mpg')
     script, div = components(p, CDN)
     sum_cost = Cost.objects.filter(publish__year=year, publish__month=month).aggregate(Sum('value'))
     min_cost = Cost.objects.filter(publish__year=year, publish__month=month).aggregate(Min('value'))
@@ -61,5 +56,4 @@ def stats_detail(request, year, month):
                                                                'max_cost': max_cost['value__max'],
                                                                'avg_cost': avg_cost['value__avg'],
                                                                'script': mark_safe(script),
-                                                               'div': mark_safe(div),
-                                                               'y': y})
+                                                               'div': mark_safe(div)})
