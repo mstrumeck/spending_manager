5ffc958d77f413d59e68f2be0f95f13196aed297
Kleju0011 <michal.strumecki@wp.pl>
2017-03-11 18:45:44 +0100

    2016-03-11 18:45

--- spending_manager/settings.py
+++ spending_manager/settings.py
@@ -138,9 +138,7 @@ STATIC_URL = '/core_sm/static/'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
