19aaf5aa6471f8747896e86f8aab224cf0ce2158
Kleju0011 <michal.strumecki@wp.pl>
2017-03-11 20:35:59 +0100

    2016-03-11 20:36

--- spending_manager/wsgi.py
+++ spending_manager/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'spending_manager.settings'
