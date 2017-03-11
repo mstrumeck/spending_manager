305de6c03983d16da9cebcf234025f68757b52a6
Kleju0011 <michal.strumecki@wp.pl>
2017-03-11 18:35:36 +0100

    2016-03-11 18:35

--- spending_manager/settings.py
+++ spending_manager/settings.py
@@ -132,12 +132,14 @@ DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/core_sm/static/'
 
 # Extra places for collectstatic to find static files.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
