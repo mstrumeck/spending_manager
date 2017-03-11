047c37605caa9246b35d0129908f537922a4a7a5
Kleju0011 <michal.strumecki@wp.pl>
2017-03-11 18:07:02 +0100

    2016-03-11 18:07

--- spending_manager/settings.py
+++ spending_manager/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from django.core.urlresolvers import reverse_lazy
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +130,19 @@ LOGIN_REDIRECT_URL = reverse_lazy('core_sm:current_detail')
 LOGIN_URL = reverse_lazy('core_sm:login')
 LOGOUT_URL = reverse_lazy('core_sm:logout')
 
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/core_sm/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
