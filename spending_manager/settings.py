6f5768444cf41760a5c44733105c0cfd07af58f9
Kleju0011 <michal.strumecki@wp.pl>
2017-07-29 13:59:42 +0200

    29-07-2017 13:59

--- spending_manager/settings.py
+++ spending_manager/settings.py
@@ -147,4 +147,5 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
